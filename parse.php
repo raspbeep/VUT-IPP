6db289dc29ed2f6b079d9b1f03b67f92872a9a42
Pavel Kratochvil <xkrato61@vutbr.cz>
2022-02-16 20:06:30 +0100

    Further work

--- parse.php
+++ parse.php
@@ -3,41 +3,142 @@
 
 ini_set('display_errors','STDERR');
 
+/* PREDEFINED ERROR CODES */
+// missing parameter, invalid parameter combination
+const ERROR_PARAM = 10;
+// error opening input files(non-existent, insufficient privileges)
+const ERROR_FILE_IN = 11;
+// error opening output files(insufficient privileges, write error)
+const ERROR_FILE_OUT = 12;
+// internal error
+const ERROR_INT = 99;
+// missing or invalid header in input file
+const ERROR_HEADER = 21;
+// unknown or incorrect opcode in input file
+const ERROR_CODE = 22;
+// lexical or syntactical error in source file
+const ERROR_PARSE = 23;
 
 
-const ERROR_PARAM = -2;
+/* IMPLEMENTATION DEPENDENT ERROR CODES */
+
 const ERROR_EMPTY = -3;
-$myFile = fopen("prog.txt", "r");
 
-parseInputArguments();
-
-$doc = null;
-try {
-    xmlInit($doc);
-} catch (Exception $e) {
-    print $e->getMessage();
+// strips line from comment
+function lineStripComment($line): string {
+    $result = strpos($line, '#');
+    if($result != false){
+        return substr($line, 0, $result);
+    }
+    return $line;
 }
 
-parseLines();
+// clears line from comment, trims
+function lineClearAndSplit($line): array {
+    return preg_split('/\s+/', lineStripComment(trim($line, "\n")));
+}
+
+// checks if line is empty or a comment(begins with '#')
+function lineIsEmpty($line): bool {
+    if ($line == '\n' || str_starts_with($line, '#')) {
+        return true;
+    }
+    return false;
+}
+
+// check for header correctness
+function processHeader() {
+    // TODO: STDIN
+    global $myFile;
+    $line = trim(fgets($myFile), " \n");
+
+    // die if header is not present
+    if(lineIsEmpty($line)) {
+        handleError(ERROR_HEADER);
+    }
+
+    // check for regex match, die if not found
+    if(!preg_match("/^\.IPPcode22$/", $line, $matches)) {
+        handleError(ERROR_HEADER);
+    }
+}
+
+function processInstruction($line) {
+    $line[0] = strtoupper($line[0]);
+
+    switch($line[0]) {
+        /* FRAMES, FUNCTION CALLS */
+        case "MOVE":
+        case "CREATEFRAME":
+        case "PUSHFRAME":
+        case "POPFRAME":
+        case "DEFVAR":
+        case "CALL":
+        case "RETURN":
+
+        /* STACK */
+        case "PUSHS":
+        case "POPS":
+
+        /* ARITHMETIC, RELATIONAL, BOOLEAN, CONVERSION*/
+        case "ADD":
+        case "SUB":
+        case "MUL":
+        case "IDIV":
+        case "LT" || "GT" || "EQ":
+        case "AND" || "OR" || "NOT":
+        case "INT2CHAR":
+        case "STRI2INT":
+
+        /* INPUT, OUTPUT */
+        case "CONCAT":
+        case "STRLEN":
+        case "GETCHAR":
+        case "SETCHAR":
+
+        /* TYPES */
+        case "TYPE":
+
+        // TODO: preklad
+        /* FLOW */
+        case "LABEL":
+        case "JUMP":
+        case "JUMPIFEQ":
+        case "JUMPIFNEQ":
+        case "EXIT":
+
+        // TODO: preklad
+        /* LADENIE */
+        case "DPRINT":
+        case "BREAK":
+    }
+}
 
 
 function parseLines() {
-
+    // TODO: STDIN
     global $myFile;
-    $line = fgets($myFile);
 
-    // when input is empty, exits with ERROR_EMPTY
-    if(!$line) {
-        handleError(ERROR_EMPTY);
+    // parse first line
+    processHeader();
+
+
+    // initialising line index for output xml order attribute
+    $lineCounter = 1;
+
+    // TODO: STDIN
+    // parsing all lines
+    while($line = fgets($myFile)) {
+
+        // skip if line is empty or comment
+        if (lineIsEmpty($line)) continue;
+
+        $line = lineClearAndSplit($line);
+
+
+        $lineCounter++;
     }
 
-
-    preg_match("^\.(IPPcode22)(IPPcode22)\n$", $line, $matches);
-    if($matches == NULL) {
-        print "no matches bruh";
-    } else {
-        print $matches;
-    }
 
 }
 
@@ -49,9 +150,10 @@ function xmlInit(&$doc) {
     $doc = new DOMDocument("1.0", "UTF-8");
     $doc->formatOutput = true;
     try{
+        // create root element in xml
         $root_elem = $doc->createElement("program");
         $root_elem = $doc->appendChild($root_elem);
-
+        // create child of root element
         $lang_attr = $doc->createAttribute("language");
         $lang_attr->value = "IPPcode22";
 
@@ -64,18 +166,21 @@ function xmlInit(&$doc) {
 
 // parses input arguments, only acceptable is --help
 function parseInputArguments(): int {
-    global $argc;
-    $argOptions = array("help");
-    $givenParams = getopt(null, $argOptions);
 
-    print $argc;
-    if($argc == 2 && isset($args["help"])) {
-        // TODO: complete a help message
-        fputs(STDOUT, "Arbitrary help message.\n");
-        return 0;
-    } else if($argc < 2) {
+    $argOptions = array("help");
+    $givenParams = getopt("", $argOptions);
+
+    global $argc;
+    if(array_key_exists("help", $givenParams)) {
+        if ($argc == 2) {
+            // TODO: complete a help message
+            fputs(STDOUT, "Arbitrary help message.\n");
+            return 0;
+        }
         handleError(ERROR_PARAM);
-    }
+    }/* else if($argc > 2) {
+        handleError(ERROR_PARAM);
+    }*/
     return 0;
 }
 
@@ -83,12 +188,33 @@ function parseInputArguments(): int {
 function handleError(int $errno) {
     switch ($errno) {
         case ERROR_PARAM:
-            fputs(STDERR, "Invalid number of input arguments.\n");
-            die(ERROR_PARAM);
+            // TODO: suggest using --help
+            fputs(STDERR, "Invalid number of input arguments. Use argument --help for use \n");
+            exit(ERROR_PARAM);
         case ERROR_EMPTY:
             fputs(STDERR, "Empty file, nothing to read.\n");
             exit(ERROR_EMPTY);
+        case ERROR_HEADER:
+            fputs(STDERR, "Invalid header line.\n");
+            exit(ERROR_HEADER);
     }
 }
+
+
+// TODO: STDIN
+$myFile = fopen("prog.txt", "r");
+
+parseInputArguments();
+
+// global variable of output document
+$doc = null;
+
+try {
+    xmlInit($doc);
+} catch (Exception $e) {
+    print $e->getMessage();
+}
+
+parseLines();
 
 
